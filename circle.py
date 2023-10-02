6368e27ac08134dc04e26a26d560fc251c5c33ef
konys123 <145491234+konys123@users.noreply.github.com>
2023-10-02 20:20:49 +0300

    Update circle.py
    added description to functions

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''принимает число r, возвращает площадь круга с радиусом r'''
     return math.pi * r * r
-
+    
 
 def perimeter(r):
+    '''принимает число r, возвращает периметр круга с радиусом r'''
     return 2 * math.pi * r
 
