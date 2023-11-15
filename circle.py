be882eff87907811d515ca49c3c9cce2d0ae3894
GLEB <govnozalypa321@gmail.com>
2023-11-15 15:30:42 +0300

    ...

--- circle.py
+++ circle.py
@@ -1,7 +1,4 @@
 import math
-import unittest
-
-
 def area(r):
     '''принимает число r, возвращает площадь круга с радиусом r'''
     return math.pi * r * r
@@ -10,17 +7,3 @@ def area(r):
 def perimetr(r):
     '''принимает число r, возвращает периметр круга с радиусом r'''
     return 2 * math.pi * r
-
-
-class CircleTest(unittest.TestCase):
-    def test_zero_area(self):
-        res = area(0)
-        self.assertEqual(res, 0)
-
-    def test_area(self):
-        res = area(1)
-        self.assertEqual(res, math.pi)
-
-    def test_zero_perimetr(self):
-        res = perimetr(0)
-        self.assertEqual(res, 0)
--- square.py
+++ square.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 def area(a):
     '''принимает число a, возвращает площадь квадрата со стороной a'''
     return a * a
@@ -9,17 +6,3 @@ def area(a):
 def perimetr(a):
     '''принимает число a, возвращает периметр квадрата со стороной a'''
     return 4 * a
-
-
-class SquareTest(unittest.TestCase):
-    def test_zero_area(self):
-        res = area(0)
-        self.assertEqual(res, 0)
-
-    def test_area(self):
-        res = area(10000)
-        self.assertEqual(res, 100000000)
-
-    def test_zero_perimetr(self):
-        res = perimetr(0)
-        self.assertEqual(res, 0)
